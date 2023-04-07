425504ad8a9aeb8ecd9f9e69dcb310968861d358
Kenneth Fukizi <ken.fukizi@afrikancoder.com>
2023-04-07 13:46:38 +0200

    Reference entity abstract added

---
+++ Kernel/ReferenceEntity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kernel
+{
+    public abstract class ReferenceEntity : AggregateRoot
+    {
+        protected ReferenceEntity() 
+        {
+        }
+        protected ReferenceEntity(string displayName)
+        {
+            DisplayName = displayName ?? string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return DisplayName;
+        }
+
+        public static IEnumerable<T> GetAll<T>() where T : ReferenceEntity
+        {
+            var type = typeof(T);
+            var valuesField = type.GetFields();
+
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)
+                .Where(fi => fi.FieldType == typeof(T));
+
+            foreach (var info in fields)
+            {
+                yield return info.GetValue(null) as T;               
+            }
+        }
+
+        public string DisplayName { get; protected set; }
+    }
+}
